ee8b76853fc11aabc709c43f6560160b9464f658
Andrew Assarattanakul <assarata@gmail.com>
2015-12-31 15:08:53 +0000

    Removed a console.log

--- routes/catalog_items.js
+++ routes/catalog_items.js
@@ -6,7 +6,6 @@ var CatalogItem = require('../models/catalog_item').Model;
 router.route('/')
   .get(function(req, res, next) {
     CatalogItems.forge().query({where: {user_id: req.user.get('id')}}).fetch().then(function(collection) {
-      console.log('collection', collection);
       res.json({collection: collection});
     }).catch(next);
   })
